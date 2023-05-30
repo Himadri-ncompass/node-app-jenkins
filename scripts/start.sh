ca0974217300a74f57151fd5c381a01424c847ad
Himadri Das <himadri@ncompass.inc>
2023-05-30 17:33:25 +0530

    added files

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,10 +1,10 @@
 
 #!/bin/bash
-if [ -d "/home/ubuntu/himadri" ]; then
-  rm -rf /home/ubuntu/himadri
-fi
+# if [ -d "/home/ubuntu/himadri" ]; then
+#   rm -rf /home/ubuntu/himadri
+# fi
 # unzip /home/ubuntu/himadri.zip -d /home/ubuntu
 # rm /home/ubuntu/himadri.zip
 # cd /home/ubuntu/himadri
 
-tmux new-session -d -s node-app 'npm start'
+# tmux new-session -d -s node-app 'npm start'
