2f9b1cfaf24907bbd6e582e0238876db99f3ecc8
Himadri Das <himadri@ncompass.inc>
2023-05-31 19:10:39 +0530

    added files

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,15 @@
+
 project_path="himadri"
 session_name="node-app"
+mkdir $project_path
 
 cd $project_path
+ unzip himadri-app.zip
+ rm -rf himadri-app.zip
  if [ $? -eq 0 ]
  then
   sudo tmux kill-session -t $session_name
-  # cd ..
-  # rm -rf /home/ubuntu/himadri
+ 
     
  fi
 
